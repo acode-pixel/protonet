1504783704e68691eeb27eafbb60972830ca6822
acode-pixel <donaldanthony245@gmail.com>
2024-12-23 14:36:49 +0800

    modified:   tests/core_sendPck.cpp

--- tests/core_sendPck.cpp
+++ tests/core_sendPck.cpp
@@ -10,6 +10,8 @@ extern "C" {
 uv_tcp_t Server;
 Protonet* loop1;
 uv_thread_t thread1;
+uv_cond_t cond1, cond2;
+uv_mutex_t mutex;
 
 void on_connect(uv_connect_t *req, int status){
     sendPck(req->handle, NULL, "bob", 1, (void*)"packet test", 0);
@@ -22,7 +24,9 @@ void alloc_cb(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf) {
 
 void read_cb(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf){
     if(nread > 0){
-        exit(0);
+        uv_mutex_lock(&mutex);
+        uv_cond_signal(&cond2);
+        uv_mutex_unlock(&mutex);
     } else {
         exit(-1);
     }
@@ -38,6 +42,7 @@ void on_connection(uv_stream_t *server, int status){
 
 void loop_thread(void* arg) {
     sockaddr_in addr;
+    printf("Statrting thread [%s] %lu\n", (char*)(static_cast<uv_loop_t*>(arg)->data), uv_thread_self());
     if(strcmp((char*)(static_cast<uv_loop_t*>(arg)->data), "client") == 0){
         uv_loop_t* loop1 = (uv_loop_t*)arg;
         uv_connect_t req;
@@ -54,7 +59,9 @@ void loop_thread(void* arg) {
         uv_ip4_addr("127.0.0.1", 5657, &addr);
         uv_tcp_bind(&Server, (struct sockaddr*)&addr, 0);
         uv_listen((uv_stream_t*)&Server, 10, on_connection);
-        uv_thread_create(&thread1, loop_thread, loop1->loop);
+        uv_mutex_lock(&mutex);
+        uv_cond_signal(&cond1);
+        uv_mutex_unlock(&mutex);
         uv_run(static_cast<uv_loop_t*>(arg), UV_RUN_DEFAULT);
     }
 }
@@ -64,11 +71,18 @@ int main(int argc, char** argv){
     Protonet* loop2 = Init();
 
     uint64_t time = uv_hrtime();
+    uv_cond_init(&cond1);
+    uv_cond_init(&cond2);
 
     uv_loop_set_data(loop1->loop, (void*)"client");
     uv_loop_set_data(loop2->loop, (void*)"server");
 
     uv_thread_t thread2;
+    uv_mutex_lock(&mutex);
     uv_thread_create(&thread2, loop_thread, loop2->loop);
-    uv_thread_join(&thread2);
+    uv_cond_wait(&cond1, &mutex);
+    uv_thread_create(&thread1, loop_thread, loop1->loop);
+    uv_sleep(250);
+    uv_cond_wait(&cond2, &mutex);
+    exit(0);
 }
