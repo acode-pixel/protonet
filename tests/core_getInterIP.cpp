a38efd6f1f281c1914d45079e46ae0558ec28572
acode-pixel <donaldanthony245@gmail.com>
2024-12-29 16:31:52 +0800

    modified:   tests/core_getInterIP.cpp

--- tests/core_getInterIP.cpp
+++ tests/core_getInterIP.cpp
@@ -6,15 +6,15 @@
 #ifdef __unix__
     #define INTER "lo"
 #else
-    #define INTER "WiFi"
+    #define INTER "Loopback Pseudo-Interface 1"
 #endif
 
 int main(int argc, char** argv){
     uv_interface_address_t inter = getInterIP(INTER);
     char ip[INET_ADDRSTRLEN];
     uv_ip4_name(&inter.address.address4, ip, INET_ADDRSTRLEN);
-    log_info("IP: %s", ip);
-    if(strcmp(ip, "127.0.0.1") == 0){
+    printf("IP: %s", ip);
+    if(strcmp(ip, "127.0.0.1") == 0 || strcmp(ip, "0.0.0.0") == 0){
         return 0;
     }
     return -1;
